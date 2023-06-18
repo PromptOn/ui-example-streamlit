7b0c10e06a9e9fae988084dee5bfa74b6f6d9f9f
szerintedmi <szerintedmi@gmail.com>
2023-06-18 07:54:39 +0100

    env selector

--- auth.py
+++ auth.py
@@ -2,41 +2,49 @@ import streamlit as st
 from prompton import errors as prompton_errors
 from prompton import client as prompton_client
 
-prompton_env = "http://127.0.0.1:8000"
-# prompton_env = "https://staging.api.prompton.ai"
+PROMPTON_ENVS = [
+    "https://staging.api.prompton.ai",
+    "http://127.0.0.1:8000",
+    "https://api.prompton.ai",
+]
 
 
 def get_prompton():
     prompton_api = prompton_client.PromptonApi(
-        environment=prompton_env, token=st.session_state["auth_token"]
+        environment=st.session_state["prompton_env"],
+        token=st.session_state["auth_token"],
     )
     return prompton_api
 
 
 def login():
-    st.session_state["prompton_env"] = prompton_env
-
     if "auth_token" not in st.session_state:
         st.session_state["auth_token"] = None
 
     if "nav_selection" not in st.session_state:
         st.session_state["nav_selection"] = None
 
+    if "prompton_env" not in st.session_state:
+        st.session_state["prompton_env"] = None
+
     if st.session_state["auth_token"]:
         return True
     else:
-        prompton = prompton_client.PromptonApi(environment=prompton_env)
-
         with st.form("Login"):
             st.write("## Prompton Login ")
-            st.write(prompton_env)
+            prompton_env = st.selectbox("Environment", PROMPTON_ENVS)
+
+            st.session_state["prompton_env"] = prompton_env
+
             email = st.text_input("email", key="email")
             password = st.text_input("Password", type="password", key="password")
 
             submitted = st.form_submit_button("Login")
 
-            if submitted:
+            if submitted and prompton_env:
                 try:
+                    prompton = prompton_client.PromptonApi(environment=prompton_env)
+
                     with st.spinner("Authenticating..."):
                         token = prompton.authentication.get_access_token(
                             username=email, password=password
