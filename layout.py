7a4d902f49cd30252905720041d4a8db14811302
szerintedmi <szerintedmi@gmail.com>
2023-06-18 07:44:18 +0100

    user info to sidebar

--- layout.py
+++ layout.py
@@ -11,27 +11,26 @@ def show():
 
 
 def show_header():
-    if st.session_state["auth_token"]:
-        prompton = auth.get_prompton()
-
-        my_org = prompton.orgs.get_current_user_org()
-        my_user = prompton.users.get_current_user()
-        my_role = my_user.role.value if my_user.role else "No role"
-
-        col1, col2, _, col3 = st.columns([10, 5, 5, 5])
-
-        col1.write(f"{my_user.email}  @  {my_org.name} ( { my_role} )")
-
-        col2.write(st.session_state["prompton_env"])
-
-        with col3:
-            logout_submitted = st.button("Logout")
-            if logout_submitted:
-                st.session_state["auth_token"] = None
-                st.experimental_rerun()
+    pass
 
 
 def show_sidebar():
     with st.sidebar:
         choose = option_menu("PromptOn", ["Inferences", "Prompts"], default_index=0)
         st.session_state["nav_selection"] = choose
+
+        if st.session_state["auth_token"]:
+            prompton = auth.get_prompton()
+
+            my_org = prompton.orgs.get_current_user_org()
+            my_user = prompton.users.get_current_user()
+            my_role = my_user.role.value if my_user.role else "No role"
+
+            st.write(my_user.email)
+            st.write(f" { my_role} @ {my_org.name}")
+            st.write("Env: ", st.session_state["prompton_env"])
+
+            logout_submitted = st.button("Logout")
+            if logout_submitted:
+                st.session_state["auth_token"] = None
+                st.experimental_rerun()
