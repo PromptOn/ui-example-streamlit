00066698f8e2cfeec49adf876d79bf4df0892c76
szerintedmi <szerintedmi@gmail.com>
2023-06-30 20:54:54 +0100

    preserve path after google sign in redirects

--- components/google_login.py
+++ components/google_login.py
@@ -1,13 +1,19 @@
 import json
 import streamlit as st
-from streamlit.web.server.websocket_headers import _get_websocket_headers
+from streamlit_javascript import st_javascript
+
 from utils.prompton_helpers import load_user
 
-from urllib.parse import urlencode
+from urllib.parse import urlencode, urlparse, parse_qs, urlunparse
+
+
+# NB: this where we reached the limits of Streamlit:
+#      streamlit encapsulates page in an iframe in hosted env - too many hacks to get this working
+#        and still need to open google sign in new window
 
 
 def show_google_login():
-    prompton_env = st.session_state.get("prompton_env")
+    prompton_env = st.session_state.get("selectbox_prompton_env")
 
     if prompton_env is not None:
         query_params = st.experimental_get_query_params()
@@ -23,15 +29,16 @@ def show_google_login():
             st.experimental_rerun()
 
         else:
-            headers = _get_websocket_headers()
+            current_url_str = st_javascript(
+                "await fetch('').then(r => window.parent.location.href)"
+            )
+            current_url = urlparse(current_url_str)
 
-            oauth_request_params = {}
-            if headers and headers.get("Origin"):
-                # oauth_request_params = f'?logged_in_redirect_uri={headers.get("Origin")}&prompton_env={prompton_env}'
-                oauth_request_params = {
-                    "logged_in_redirect_uri": headers.get("Origin"),
-                    "prompton_env": prompton_env,
-                }
+            oauth_request_params = {
+                # "logged_in_redirect_uri": headers.get("Origin"),
+                "logged_in_redirect_uri": f"{current_url.scheme}://{current_url.netloc}{current_url.path}",
+                "prompton_env": prompton_env,
+            }
 
             login_query_params = urlencode(
                 {**query_params, **oauth_request_params},
